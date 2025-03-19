344faece247d03c3f2ba3430a080619def8aef5e
Rigel <rigel.rahateen@student.president.ac.id>
2025-03-19 11:10:30 +0700

    layout and style customization

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
 import { RightEmpty } from "./components/layout/Empty";
-import { LeftSidebar } from "./components/layout/Sidebar";
-import { AboutSection } from "./components/sections/About";
-import { ExperienceSection } from "./components/sections/Experience";
-import { ProjectsSection } from "./components/sections/Projects";
-import { SkillsSection } from "./components/sections/Skills";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen">
-      {/* Left sidebar (1/4) */}
-      <div className="w-1/6">
-        <LeftSidebar />
-      </div>
-      
-      {/* Main content (1/2) */}
-      <div className="w-2/3 h-screen overflow-y-auto py-6 px-10">
-        <AboutSection />
-        <SkillsSection />
-        <ExperienceSection />
-        <ProjectsSection />
-      </div>
-      
-      {/* Right sidebar for future use (1/4) */}
-      <div className="w-1/6">
-        <RightEmpty />
-      </div>
+    <div className="flex flex-col justify-center w-full">
+      <RightEmpty />
     </div>
   )
 }
