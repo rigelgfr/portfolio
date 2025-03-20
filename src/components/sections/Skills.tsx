a42d4a7205972ba063f24e8e852153093aa35335
Rigel <rigel.rahateen@student.president.ac.id>
2025-03-20 17:00:14 +0700

    feat: reusable section header component

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -3,21 +3,18 @@ import { FaWrench } from "react-icons/fa"
 import { Separator } from "../ui/separator"
 import { skills } from "@/data/skills"
 import { SkillCard } from "../ui/section-card"
+import { SectionHeader } from "../ui/section-header"
 
 export function SkillsSection() {
   return (
     <SectionContainerBorderless id="skills">
       <div className="flex-col space-y-3">
-        <span className="flex items-center gap-2">
-      
-            <FaWrench className="w-4 h-4" />
-            <span className="flex gap-1 items-center">
-              <h1 className="text-md font-semibold">skills & technologies</h1>
-              <p className="text-sm text-grey-light">—</p>
-              <p className="text-sm text-grey-light">what i usually use</p>
-            </span>
-
-        </span>
+        
+        <SectionHeader
+          icon={<FaWrench className="w-4 h-4"/>}
+          title="skills & technologies"
+          description="what i use, still learning and improving"
+        />
 
         <Separator />
 
