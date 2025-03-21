8fb2c7d75c3044ca87b0dbe78818e6184afcde95
Rigel <rigel.rahateen@student.president.ac.id>
2025-03-21 19:53:53 +0700

    feat: space background

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,18 @@ import { SkillsSection } from "./components/sections/Skills";
 import { ExperienceSection } from "./components/sections/Experience";
 import { Toaster } from "./components/ui/sonner";
 import { ProjectsSection } from "./components/sections/Projects";
+import Starfield from 'react-starfield';
 
 export default function Page() {
   return (
     <main className="flex justify-center">
+      <Starfield
+        starCount={750}
+        starColor={[255, 255, 255]}
+        speedFactor={0.05}
+        backgroundColor="black"
+      />
+
       <div className="w-full sm:w-3/4 xl:w-[800px]">
         <Content />
         <AboutSection />
