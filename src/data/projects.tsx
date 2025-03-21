4a2b0deaad38ddff1294d1f7ca2f8f2227bab3b4
Rigel Gregory <rigel.rahateen@student.president.ac.id>
2025-03-21 15:23:06 +0700

    refactor: added stack

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,13 +1,13 @@
 import { ProjectCardProps } from "@/components/ui/section-card";
 import { FaNodeJs, FaReact } from "react-icons/fa";
-import { SiAxios, SiExpress, SiGenius, SiGooglegemini, SiNextdotjs, SiPostgresql, SiTailwindcss, SiTypescript } from "react-icons/si";
+import { SiAxios, SiExpress, SiGenius, SiGooglegemini, SiNextdotjs, SiPostgresql, SiPrisma, SiTailwindcss, SiTypescript } from "react-icons/si";
 
 export const STACKS = {
     // Frontend stacks
     REACT: { icon: <FaReact />, stack: "React", frontend: true },
     NEXTJS: { icon: <SiNextdotjs />, stack: "Next.js", frontend: true },
     TYPESCRIPT: { icon: <SiTypescript />, stack: "TypeScript", frontend: true },
-    TAILWIND: { icon: <SiTailwindcss />, stack: "Tailwind CSS", frontend: true },
+    TAILWIND: { icon: <SiTailwindcss />, stack: "Tailwind", frontend: true },
   
     // Backend stacks
     NODE: { icon: <FaNodeJs />, stack: "Node.js", frontend: false },
@@ -16,13 +16,14 @@ export const STACKS = {
     AXIOS: { icon: <SiAxios />, stack: "Axios", frontend: false },
     GEMINI: { icon: <SiGooglegemini />, stack: "Gemini", frontend: false },
     GENIUS: { icon: <SiGenius />, stack: "Genius", frontend: false },
+    PRISMA: { icon: <SiPrisma />, stack: "Prisma", frontend: false }
   }
 
 export const projects: ProjectCardProps[] = [
     {
         app_name: "UtacyLyrics",
         formality: "assignment, solo",
-        description: "A lyrics web app that allows romanization of Japanese lyrics (characters) and more by leveraging Gemini API.",
+        description: "a lyrics web app that allows romanization of Japanese lyrics (characters) and more by leveraging Gemini API.",
         job: "Frontend Developer",
         image: [
             "https://github.com/rigelgfr/utacy-lyrics/blob/main/src/img/screenshots/3.png?raw=true",
@@ -40,4 +41,23 @@ export const projects: ProjectCardProps[] = [
             STACKS.GEMINI
         ]
     },
+    {
+        app_name: "BRJ-WRM",
+        formality: "client, solo",
+        description: "warehouse report management (WRM) is an internal web app that displays the company's inbound and outbound data as charts and tables.",
+        job: "Fullstack Developer",
+        image: [
+            "https://github.com/rigelgfr/brj-wrm/blob/master/public/ss/3.png?raw=true",
+            "https://github.com/rigelgfr/brj-wrm/blob/master/public/ss/2.png?raw=true",
+            "https://github.com/rigelgfr/brj-wrm/blob/master/public/ss/1.png?raw=true",
+        ],
+        source_code: "https://github.com/rigelgfr/brj-wrm",
+        stack: [
+            STACKS.NEXTJS,
+            STACKS.TYPESCRIPT,
+            STACKS.TAILWIND,
+            STACKS.POSTGRESQL,
+            STACKS.PRISMA
+        ]
+    }
 ]
