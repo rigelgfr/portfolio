413de4c8f5e03fbb9f430797b43e298ebe9838e8
Rigel Gregory <rigel.rahateen@student.president.ac.id>
2025-04-15 09:41:08 +0700

    style

--- src/components/ui/section-header.tsx
+++ src/components/ui/section-header.tsx
@@ -16,9 +16,11 @@ export function SectionHeader({
   const titleContent = (
     <span className="flex items-center gap-2">
       <span className="flex gap-1 items-center">
-        <h1 className="text-sm sm:text-md md:text-lg font-semibold">{title}</h1>
-        <p className="text-sm sm:text-md md:text-md text-grey-light">—</p>
-        <p className="text-sm sm:text-md md:text-md text-grey-light ">{description}</p>
+        <h1 className="font-semibold text-sm sm:text-md md:text-lg">{title}</h1>
+        <div className="flex justify-center items-end gap-1">
+          <p className="text-grey-light text-sm md:text-md">—</p>
+          <p className="text-grey-light text-sm md:text-md">{description}</p>
+        </div>
       </span>
     </span>
   );
