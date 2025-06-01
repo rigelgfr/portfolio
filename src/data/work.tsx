a2ff3a2f520cb8d369d175615a976b262169fafd
Rigel <rigel.rahateen@student.president.ac.id>
2025-06-01 10:49:02 +0700

    update: work exp

--- src/data/work.tsx
+++ src/data/work.tsx
@@ -2,10 +2,22 @@ import { ExperienceCardRowProps } from "@/components/ui/section-card";
 
 export const work: ExperienceCardRowProps[] = [
     {
+        image: "https://media.licdn.com/dms/image/v2/D560BAQENO2SsEquYGQ/company-logo_200_200/B56ZX8jqYqGQAY-/0/1743698962876?e=1754524800&v=beta&t=wSgex5Tr3NtvDs4pnyuzmKcXkajZGyEdEgBdFWt7Wds",
+        institute: "PT. EatSight Nusantara",
+        link: "https://www.linkedin.com/company/eatsight-nusantara/",
+        period: "May 2025 - Present",
+        work: "Frontend Web Developer",
+        points: [
+            "Translated Figma designs into modular, reusable React components",
+            "Implemented responsive design principles to ensure optimal user experience across devices",
+            "Collaborated with backend developers to integrate RESTful APIs for dynamic data handling",
+        ]
+    },
+    {
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaasC-9HOBrUZoOlKjAvM1VIMpdAMqzFS0qA&s",
         institute: "PT. Bimaruna Jaya",
         link: "https://korindo-brj.com/",
-        period: "Oct 2024 - Present",
+        period: "Oct 2024 - May 2025",
         work: "Fullstack Web Developer Intern",
         points: [
             "Developed a local web application to help visualize warehouse data for report and presentation purposes",
